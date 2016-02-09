f588c88242ade33b3afbdbab6af1908a4422217c
jorge305 <jvaldes305@gmail.com>
2016-02-09 15:16:05 -0500

    Create index_dom.js

---
+++ index_dom.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<!-- <script src="functions.js"></script> -->
+
+
+
+<head>
+
+	<title>vhjvj
+	</title>
+
+</head>
+
+<body>
+	<div>
+		<p>
+			<a href="www.google.com">here</a>
+		</p>
+
+		<p>hgcghc
+		</p>
+
+		<div id="back">
+
+		</div>
+
+		<div id="time">
+
+		</div>
+
+		<div id="todo">
+			<ul id="demo">
+				<h1>TO DO LIST</h1>
+			</ul>
+
+		</div>
+
+
+
+
+	</div>
+
+</body>
+<script src="doms.js"></script>
+Javascript or &ltHTML&gt
